41fe1d16c889912791f24b668cb4b90089cfb7a7
Rubinshtein-Ilya <109314003+Rubinshtein-Ilya@users.noreply.github.com>
2024-03-12 17:20:39 +0600

    Update checkbox.php

--- docs/checkbox.php
+++ docs/checkbox.php
@@ -6,11 +6,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $postData = file_get_contents('php://input');
     $data = json_decode($postData, true);
 
-    // Проверяем наличие обязательных полей
-    // if (!isset($data['selectedCheckboxes']) || empty($data['selectedCheckboxes'])) {
-    //     echo json_encode(['error' => 'Не выбраны чекбоксы']);
-    //     exit;
-    // }
+    Проверяем наличие обязательных полей
+    if (!isset($data['selectedCheckboxes']) || empty($data['selectedCheckboxes'])) {
+        echo json_encode(['error' => 'Не выбраны чекбоксы']);
+        exit;
+    }
 
     // Формируем сообщение для отправки на почту
     $selectedCheckboxes = json_decode($data['selectedCheckboxes']);
@@ -24,14 +24,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Адрес почты, на который отправляем сообщение
-    $to = 'fahraziev-ilgiz@yandex.ru';
+    $to = 'some@yandex.ru';
 
     // Тема письма
     $subject = 'Заявка с сайта';
 
     // Дополнительные заголовки
-    $headers = 'From: fahraziev-ilgiz@yandex.ru' . "\r\n" .
-        'Reply-To: fahraziev-ilgiz@yandex.ru' . "\r\n" .
+    $headers = 'From: some@yandex.ru' . "\r\n" .
+        'Reply-To: some@yandex.ru' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
     // Отправляем сообщение на почту
